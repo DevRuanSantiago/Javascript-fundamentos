8d3d38543036b8caa4d1cb64932eb1013b29fb07
Ruan gonçalves santiago <123724861+estatisticoo@users.noreply.github.com>
2025-06-04 15:54:51 -0300

    Primeiros passos e boas praticas em javascript

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let js = 'amazing';
+if (js === 'amazing') alert("javascript is fun");
+console.log('jonas');
+let firstName = "jonathan";
+alert(firstName)
+let myFirstJob = 'programmer';
+let myLastJob = "help desk";
+let country = 'Brazil';
+let continent = 'SA';
+let population =  '500.000.000';
+console.log(country,continent,population)
+let javascriptIsFun = true
+alert(javascriptIsFun)
+console.log(typeof javascriptIsFun)
