e6f51a9d30419f5d9eadaa03785c32c6cd11bce6
Ruan gonçalves santiago <123724861+estatisticoo@users.noreply.github.com>
2025-06-05 19:43:42 -0300

    Estudo sobre if e else

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,15 +20,16 @@ let age = 31;
 age = 32;
 const birthday = 2001;
 */
-
+/*
 const agejonas = 3000-500;
 console.log (agejonas);
 const now = 2500;
 const agesarah= now-500;
 console.log(agesarah);
 console.log(agejonas*2/agesarah/2);
+*/
 
-
+/*
 const firstName ='Jonas'
 const lastName = 'gabriel'
 console.log(firstName+ ' '+lastName);
@@ -41,3 +42,39 @@ console.log(x)
 //comparison operators
 console.log(agejonas>agesarah);
 console.log(agesarah>= 18);
+*/
+/*
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthday = 2001;
+const year = 2050;
+const jonas = "i'm " + firstName +   ', a ' + (year - birthday) + ' years Old ' + job + ' ! ';
+console.log(jonas);
+const jonasnew = `i'm ${firstName}, a  ${job} i have ${year-birthday} years old` ; 
+console.log(jonasnew)
+console.log(`a regular string bro`)
+console.log('quebra de \n\ linha')
+*/
+const age =19;
+const isoldenough = age>=18;
+console.log(isoldenough);
+if(isoldenough) {
+    console.log('sara can start drive licence')
+
+}
+else{
+    const yearsleft = (18-age);
+    console.log(`sara needs to wait ${yearsleft} years until she can have a drive licence`)
+}
+/*
+const birthday = 1991;
+if(birthday<2000)
+{
+    let century = 20;
+    console.log(`century is equal to ${century}`)
+}
+else{
+    let century = 21;
+    console.log(`century is equal to ${century}`)
+}
+*/
