43100095095d110e0db21ecdbf3465e4721e8b02
DevRuanSantiago <ruangon2021@gmail.com>
2025-06-05 07:45:30 -0300

    Update script.js
    Adiciona exercicios feitos

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 if (js === 'amazing') alert("javascript is fun");
 console.log('jonas');
@@ -12,3 +13,31 @@ console.log(country,continent,population)
 let javascriptIsFun = true
 alert(javascriptIsFun)
 console.log(typeof javascriptIsFun)
+*/
+
+/*
+let age = 31;
+age = 32;
+const birthday = 2001;
+*/
+
+const agejonas = 3000-500;
+console.log (agejonas);
+const now = 2500;
+const agesarah= now-500;
+console.log(agesarah);
+console.log(agejonas*2/agesarah/2);
+
+
+const firstName ='Jonas'
+const lastName = 'gabriel'
+console.log(firstName+ ' '+lastName);
+let x = 10+5;
+console.log(x)
+x += 10;
+console.log(x);
+x *= 4;
+console.log(x)
+//comparison operators
+console.log(agejonas>agesarah);
+console.log(agesarah>= 18);
